c37fe43d575330f83f65b194d8fa80380b605ccc
Valentina Galeano <valentinagaleanow@gmail.com>
2023-03-29 17:30:00 -0500

    taller2_modificando

---
+++ taller2/comprimir_carpeta.sh
@@ -0,0 +1,2 @@
+# comprimir_carpeta.sh
+tar -czvf mi_carpeta_comprimida.tar.gz mi_carpeta
---
+++ taller2/suma_vectores.sh
@@ -0,0 +1,8 @@
+archivo_suma="suma.txt"
+
+contenido1=($(cat nueva_carpeta/archivo1.txt))
+contenido2=($(cat nueva_carpeta/archivo2.txt))
+
+suma=$((contenido1 + contenido2))
+
+echo "$suma" > "$archivo_suma"
